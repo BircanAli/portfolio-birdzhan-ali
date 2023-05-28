642f973a9bef4bea87d7a2204389c24573ff0bff
Birdzhan Ali <105841521+BircanAli@users.noreply.github.com>
2023-05-28 11:48:08 +0200

    Project page is done

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,76 +3,111 @@ import KeeperAppImg from "../../assets/projects-images/KeeperApp.png";
 import ToDoList from "../../assets/projects-images/ToDoList.png";
 import OmniFood from "../../assets/projects-images/OmniFoot — Never cook again-small.png";
 import Sociopad1 from "assets/projects-images/ReactAppHomeLightMode.png";
+import Sociolad2 from "assets/projects-images/ReactAppHomePageDarkModee.png";
 
 const Projects = () => {
   return (
     <div className="projects-container">
       <div className="project">
         <div className="img-container">
-          <a href="https://react-vite-keeper-app.netlify.app/" target="_blank">
+          <a
+            href="https://react-vite-keeper-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img className="img-project" src={KeeperAppImg} alt="project img" />
           </a>
         </div>
         <div className="project-text">
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
-            aspernatur dolorum velit quod doloribus et hic, est quam. Ratione a
-            dolor esse architecto blanditiis. Tenetur beatae aspernatur quasi
-            velit sed.t
+            My first app{" "}
+            <a
+              href="https://bircan-first-todolist.onrender.com"
+              rel="noreferrer"
+            >
+              https://bircan-first-todolist.onrender.com
+            </a>
           </p>
         </div>
       </div>
 
       <div className="project">
         <div className="img-container">
-          <a href="https://bircan-first-todolist.onrender.com" target="_blank">
+          <a
+            href="https://bircan-first-todolist.onrender.com"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img className="img-project" src={ToDoList} alt="project img" />
           </a>
         </div>
         <div className="project-text">
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
-            aspernatur dolorum velit quod doloribus et hic, est quam. Ratione a
-            dolor esse architecto blanditiis. Tenetur beatae aspernatur quasi
-            velit sed.t
+            This is my To Do List Application. If u wish you can write . . .
+            <a href="https://bircan-first-todolist.onrender.com" rel="noreffer">
+              https://bircan-first-todolist.onrender.com
+            </a>
           </p>
         </div>
       </div>
 
       <div className="project">
         <div className="img-container">
-          <a href="https://omnifood-birdzhanali.netlify.app/" target="_blank">
+          <a
+            href="https://omnifood-birdzhanali.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img className="img-project" src={OmniFood} alt="project img" />
           </a>
         </div>
         <div className="project-text">
+          <h1>My Landing page from Jonas Schmedtmann course .</h1>
+          <p>This landing page includes .</p>
+          <p>Hero section .</p>
+          <p>Testimonials .</p>
+          <p>Small gallery .</p>
+          <p>Card section .</p>
+          <p>Contact me .</p>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
-            aspernatur dolorum velit quod doloribus et hic, est quam. Ratione a
-            dolor esse architecto blanditiis. Tenetur beatae aspernatur quasi
-            velit sed.t
+            <a href="https://omnifood-birdzhanali.netlify.app/" rel="noreffer">
+              https://omnifood-birdzhanali.netlify.app/
+            </a>
           </p>
         </div>
       </div>
 
       <div className="project">
         <div className="img-container">
-          <a href="" target="_blank">
+          <a
+            href="https://github.com/BircanAli/MERN-Social-page"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img className="img-project" src={Sociopad1} alt="project img" />
           </a>
         </div>
         <div className="img-container">
-          <a href="" target="_blank">
-            <img className="img-project" src={OmniFood} alt="project img" />
+          <a
+            href="https://github.com/BircanAli/MERN-Social-page"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="img-project" src={Sociolad2} alt="project img" />
           </a>
         </div>
         <div className="project-text">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
-            aspernatur dolorum velit quod doloribus et hic, est quam. Ratione a
-            dolor esse architecto blanditiis. Tenetur beatae aspernatur quasi
-            velit sed.t
-          </p>
+          <h1>
+            This is my MERN Stak Project check it on my github:
+            <br />
+            <a
+              href="https://github.com/BircanAli/MERN-Social-page"
+              rel="noreffer"
+            >
+              https://github.com/BircanAli/MERN-Social-page
+            </a>
+          </h1>
+          <p></p>
         </div>
       </div>
     </div>
