228f68e5bf1ec411737a22311e61f05306032783
Birdzhan Ali <105841521+BircanAli@users.noreply.github.com>
2023-05-28 10:25:38 +0200

    working on project folder

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,8 @@
 import "./index.scss";
 import KeeperAppImg from "../../assets/projects-images/KeeperApp.png";
 import ToDoList from "../../assets/projects-images/ToDoList.png";
-import OmniFood from "../../assets/projects-images/OmniFoot — Never cook again.png";
+import OmniFood from "../../assets/projects-images/OmniFoot — Never cook again-small.png";
+import Sociopad1 from "assets/projects-images/ReactAppHomeLightMode.png";
 
 const Projects = () => {
   return (
@@ -41,6 +42,27 @@ const Projects = () => {
       <div className="project">
         <div className="img-container">
           <a href="https://omnifood-birdzhanali.netlify.app/" target="_blank">
+            <img className="img-project" src={OmniFood} alt="project img" />
+          </a>
+        </div>
+        <div className="project-text">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
+            aspernatur dolorum velit quod doloribus et hic, est quam. Ratione a
+            dolor esse architecto blanditiis. Tenetur beatae aspernatur quasi
+            velit sed.t
+          </p>
+        </div>
+      </div>
+
+      <div className="project">
+        <div className="img-container">
+          <a href="" target="_blank">
+            <img className="img-project" src={Sociopad1} alt="project img" />
+          </a>
+        </div>
+        <div className="img-container">
+          <a href="" target="_blank">
             <img className="img-project" src={OmniFood} alt="project img" />
           </a>
         </div>
