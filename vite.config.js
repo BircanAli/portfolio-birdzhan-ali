e60432cf4aed2b59f7daeb5cbd83eeb79402bc24
BircanAli <bircanali91@gmail.com>
2023-10-12 14:51:17 +0200

    new commit vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import pluginRewriteAll from "vite--plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [pluginRewriteAll()],
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "https://birdzhanaliprofile.netlify.app/",
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ""),
-//       },
-//     },
-//   },
-// });
