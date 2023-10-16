470ef4a4526d6fec6ea4cb9aa613bc3268ce49fc
Birdzhan Ali <Bircanali91@gmail.com>
2023-10-16 06:09:20 +0200

    new changes working on lighthouse

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ const Project = ({ url, num }) => {
   return (
     <div key={num} className="video-container">
       <video width="500px" src={url} controls>
-        <source src={url}></source>
+        <source src={url} />
       </video>
     </div>
   );
