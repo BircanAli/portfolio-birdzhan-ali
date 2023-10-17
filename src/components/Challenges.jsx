b3334ef9b900ab1861ab43c54c4cc41df0a619b4
Birdzhan Ali <Bircanali91@gmail.com>
2023-10-17 16:27:03 +0200

    images convert to webp

--- src/components/Challenges.jsx
+++ src/components/Challenges.jsx
@@ -2,11 +2,15 @@ import { Link } from "react-router-dom";
 import challengesList from "../utils/challenges";
 
 const Challenges = () => {
-  return challengesList.map(({ src, path, alt }, index) => {
+  return challengesList.map(({ src, path, alt, webp }, index) => {
     return (
       <li key={index}>
         <Link to={path} target="_blank">
-          <img src={src} alt={alt} width="600" height="400" />
+          <picture>
+            <source srcset={webp} type="image/webp" />
+            <source srcset={src} type="image/jpeg" />
+            <img src={src} alt={alt} width="600" height="400" />
+          </picture>
         </Link>
       </li>
     );
--- src/utils/challenges.js
+++ src/utils/challenges.js
@@ -8,51 +8,70 @@ import cardShoppingBag from "../assets/challange-images/card-shoping-bag.png";
 import expensesChart from "../assets/challange-images/expenses-chart.png";
 import newsLetter from "../assets/challange-images/news-letter.png";
 
+import femCardWebp from "../assets/challange-images/fem-card.webp";
+import femCardTwoWebp from "../assets/challange-images/fem-card-two.webp";
+import femAgeCalcWebp from "../assets/challange-images/fem-age-calculator.webp";
+import femComingSoonWebp from "../assets/challange-images/fem-coming-soon.webp";
+import keeperAppWebp from "../assets/challange-images/keeper-app.webp";
+import toDoListWebp from "../assets/challange-images/to-do-list.webp";
+import cardShoppingBagWebp from "../assets/challange-images/card-shoping-bag.webp";
+import expensesChartWebp from "../assets/challange-images/expenses-chart.webp";
+import newsLetterWebp from "../assets/challange-images/news-letter.webp";
+
 const challengesList = [
   {
     src: femCard,
     path: "https://hilarious-donut-f85fd6.netlify.app/",
     alt: "fem challenge card",
+    webp: { femCardWebp },
   },
   {
     src: femCardTwo,
     path: "https://hilarious-donut-f85fd6.netlify.app/",
     alt: "fem challenge card",
+    webp: { femCardTwoWebp },
   },
   {
     src: femAgeCalc,
     path: "https://age-calculator-birdzhan-ali.netlify.app/",
     alt: "fem challenge age calculator",
+    webp: { femAgeCalcWebp },
   },
   {
     src: femComingSoon,
     path: "https://coming-soon-page-birdzhan.netlify.app/",
     alt: "fem challenge coming soon page",
+    webp: { femComingSoonWebp },
   },
   {
     src: toDoList,
     path: "https://bircan-first-todolist.onrender.com/",
     alt: "to do list",
+    webp: { toDoListWebp },
   },
   {
     src: keeperApp,
     path: "https://react-vite-keeper-app.netlify.app/",
     alt: "notes keeper app",
+    webp: { keeperAppWebp },
   },
   {
     src: cardShoppingBag,
     path: "https://redux-card-bag.netlify.app/",
     alt: "notes keeper app",
+    webp: { cardShoppingBagWebp },
   },
   {
     src: newsLetter,
     path: "https://bircanali.github.io/fem-newsletter-sign-up-form/",
     alt: "fem news letter sing up",
+    webp: { newsLetterWebp },
   },
   {
     src: expensesChart,
     path: "https://fem-expense-chart.netlify.app/",
     alt: "fem news letter sing up",
+    webp: { expensesChartWebp },
   },
 ];
 
